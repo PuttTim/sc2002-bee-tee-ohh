9e6285c58dacb9abdbdd0e3878f08958c51cdabd
xerri <tanweixin2004@gmail.com>
2025-03-25 01:42:09 +0800

    Organised and updated user model

---
+++ src/models/User.java
@@ -0,0 +1,37 @@
+package models;
+
+public abstract class User {
+    private String userID;
+    private String name;
+    private String role;
+
+    public User(String userID, String name, String role) {
+        this.userID = userID;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getUserID() {
+        return this.userID;
+    }
+
+    public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getRole() {
+		return role;
+	}
+
+    public void setRole(String role) {
+		this.role = role;
+	}
+}
