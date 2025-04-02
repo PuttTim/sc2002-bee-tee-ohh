8aa9698247301a6cb137644d995e42edd8b58e9b
PuttTim <yannaputt@gmail.com>
2025-04-03 00:55:03 +0800

    feat: Replace Main class with App as the main entry point of the application

---
+++ src/App.java
@@ -0,0 +1,7 @@
+
+// This is the main entrypoint of our application.
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bee Tee Ohh!");
+    }    
+}
